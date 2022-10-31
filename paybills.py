61387f21ffb9e40056c0c9615a7054d9c8f10d5f
Finn Wengerd <finnian.wengerd@outlook.com>
2022-10-31 11:35:03 -0400

    adding pseudo code

--- paybills.py
+++ paybills.py
@@ -1,2 +1,35 @@
 #pseudo code 
 #This is a change
+
+"""Pseudo code
+
+Check config.txt for previous configurations.
+If new:
+    Welcome user. How many bills do we need to pay?
+    >>>User input (n)
+    Please enter the website followed by the nickname of the n bill (example https://rent.com, rent) 
+    >>>User input
+    Repeat for n times
+
+    create dictionary with values and their links (ex mylinks={"rent": "https://rent.com", "electric": https://electric.com"... }
+    
+    save dict to config.txt
+
+If preconfigured:
+    Welcome user. What bills are we paying today?
+    list keys from mylinks and option to add a new key (sort alphabetical?)
+
+
+
+def addBill()
+    key = user input 
+    value = user input 
+    append mylinks("a":"b") 
+
+#Open value webpage where a is the key from mylinks
+def openBill(a)
+    
+    
+
+
+"""
