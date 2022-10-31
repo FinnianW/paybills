e7efcd1a341ab9f560e1a961d7f5520414bdff92
Finn Wengerd <finnian.wengerd@outlook.com>
2022-10-31 19:43:17 -0400

    open, read, and search config.txt for previous use based on dictionary created

--- paybills.py
+++ paybills.py
@@ -4,6 +4,21 @@
 """Pseudo code
 
 Check config.txt for previous configurations.
+"""
+configContent = ""
+
+with open ("config.txt", "r+") as file:
+    configContent = file.read()
+    print(configContent)
+
+def programUse(configContent):
+    if "billList" in configContent:
+        print("programUse True")
+        return True
+    else:
+        print("programUse False")
+        return False
+"""
 If new:
     Welcome user. How many bills do we need to pay?
     >>>User input (n)
@@ -11,37 +26,35 @@ If new:
     >>>User input
     Repeat for n times
 
-    create dictionary with values and their links (ex mylinks={"rent": "https://rent.com", "electric": https://electric.com"... }
+    create dictionary with values and their links (ex billLinks={"rent": "https://rent.com", "electric": https://electric.com"... }
     
     save dict to config.txt
 
 If preconfigured:
     Welcome user. What bills are we paying today?
-    list keys from mylinks and option to add, delete, or change a key and value (sort alphabetical?)
+    list keys from billLinks and option to add, delete, or change a key and value (sort alphabetical?)
 
-#Open value webpage where a is the key from mylinks
+#Open value webpage where a is the key from billLinks
 def openBill(a)
-    open mylinks{a} #get value of key a
+    open billLinks{a} #get value of key a
 
 #Create a new key:value pair
 def addBill()
     a = user input 
     b = user input 
-    append mylinks("a":"b") 
+    append billLinks("a":"b") 
 
 #Delete a key:value pair
 def removeBill()
     a = user input
-    del mylinks["a"]
+    del billLinks["a"]
 
 
-def saveConf(mylinks)
-    overwrite mylinks as mylinks in config file
+def saveConf(billLinks)
+    overwrite billLinks as billLinks in config file
 
-def closeProgram()
-    close config.txt
-
-    
-
-
+def closeProgram(f):
+    #close config.txt
+    f.close()
 """
+programUse(configContent)
